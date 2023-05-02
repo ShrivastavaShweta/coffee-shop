78279550eb34d4c22473d1eda1bc4f08905f8765
ShrivastavaShweta <117964441+ShrivastavaShweta@users.noreply.github.com>
2023-05-01 21:05:52 -0500

    Revert "Recommit user login and registration view"

--- coffee-shop-online/src/app/app.module.ts
+++ coffee-shop-online/src/app/app.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppComponent } from './app.component';
-import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
-import { RegisterUserPageComponent } from './register-user-page/register-user-page.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'user-login', component: UserLoginPageComponent },
-  { path: 'register-user', component: RegisterUserPageComponent },
-  { path: 'admin-login', component: AdminLoginComponent }
+  { path: 'admin-login', component: AdminLoginComponent },
+  
 ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginPageComponent,
-    RegisterUserPageComponent,
     AdminLoginComponent
   ],
   imports: [
@@ -30,4 +24,3 @@ const routes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
