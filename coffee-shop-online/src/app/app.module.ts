4a8bf779caa43b9a088a2301638af837e7d7729d
Meiling <happylive_2006@hotmail.com>
2023-05-03 09:52:09 -0500

    meiling

--- coffee-shop-online/src/app/app.module.ts
+++ coffee-shop-online/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+// import { RouterModule,Routes } from '@angular/router';
+// const routes: Routes=[{ path:'homepage', component:HomePageComponent}]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
