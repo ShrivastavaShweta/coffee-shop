73cb6cab0df43737c1232a6bb893bf34cf9793fd
mrjermarco <107326902+mrjermarco@users.noreply.github.com>
2023-05-04 20:52:52 -0500

    user and register page views

--- coffee-shop-online/src/app/app.module.ts
+++ coffee-shop-online/src/app/app.module.ts
@@ -1,15 +1,17 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+
 import { AdminLoginComponent } from './admin-login/admin-login.component';
-import { Routes, RouterModule } from '@angular/router';
 import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
 import { RegisterUserPageComponent } from './register-user-page/register-user-page.component';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'admin-login', component: AdminLoginComponent },
-  
+  { path: 'user-login', component: UserLoginPageComponent},
+  { path: 'register-user', component: RegisterUserPageComponent}
 ];
 
 
@@ -27,4 +29,5 @@ const routes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
