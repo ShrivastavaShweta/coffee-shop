5eda396334fcb6409e54cfc0a2d5df3702f33533
Ronnie Horn <ronniehorn@hotmail.com>
2023-05-06 23:50:01 -0500

    committing again

--- coffee-shop-online/src/app/app.module.ts
+++ coffee-shop-online/src/app/app.module.ts
@@ -1,30 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
 import { RegisterUserPageComponent } from './register-user-page/register-user-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { UserHomepageComponent } from './user-homepage/user-homepage.component';
+
 
 const routes: Routes = [
   { path: 'admin-login', component: AdminLoginComponent },
+  { path: 'home-page', component:HomePageComponent},
+  { path:'register-user', component:RegisterUserPageComponent},
+  { path:'user-login', component:UserLoginPageComponent },
   { path: 'user-homepage', component: UserHomepageComponent },
-];
-
-
+]
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
     UserLoginPageComponent,
     RegisterUserPageComponent,
+    HomePageComponent,
     UserHomepageComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
