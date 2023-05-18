da37d213d5cfa98d25148d0662b9b1b3f13325d2
Ronnie Horn <ronniehorn@hotmail.com>
2023-05-17 23:42:22 -0500

    order item specifics near done on view

--- coffee-shop-online/src/app/app.module.ts
+++ coffee-shop-online/src/app/app.module.ts
@@ -8,8 +8,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
 import { RegisterUserPageComponent } from './register-user-page/register-user-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
-
 import { UserHomepageComponent } from './user-homepage/user-homepage.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
 
 
 
@@ -19,8 +34,7 @@ const routes: Routes = [
   { path:'register-user', component:RegisterUserPageComponent},
   { path:'user-login', component:UserLoginPageComponent },
   { path: 'user-homepage', component: UserHomepageComponent },
-
-  { path:'user-login', component:UserLoginPageComponent }
+  { path:'user-login', component:UserLoginPageComponent },
 ]
 @NgModule({
   declarations: [
@@ -31,14 +45,29 @@ const routes: Routes = [
 
     HomePageComponent,
     UserHomepageComponent,
-    HomePageComponent
+    HomePageComponent,
+
+
 
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatListModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatBadgeModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
